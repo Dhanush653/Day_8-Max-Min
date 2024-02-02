9ed5807b5fa1069249401c58afa985dfc0fc6b04
Dhanush Kumar <dk652003@gmail.com>
2024-02-02 11:56:06 +0530

    Added Maximum Number Of Three

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,4 +1,4 @@
-
+// Generic class
 public class MaxNumber<T extends Comparable<T>> {
     public T findmax(T num1, T num2, T num3){
         T max = num1;
@@ -11,8 +11,12 @@ public class MaxNumber<T extends Comparable<T>> {
         return max;
     }
     public static void main(String[] args) {
-        MaxNumber<Integer> maxnum = new MaxNumber<Integer>();
+        MaxNumber<Integer> maxnum = new MaxNumber<>();
         int result = maxnum.findmax(23,45,34);
-        System.out.println(result);
+        int result1 = maxnum.findmax(67,34,49);
+        int result2 = maxnum.findmax(87,38,89);
+        System.out.println("The Maximum Number is: "+result);
+        System.out.println("The Maximum Number is: "+result1);
+        System.out.println("The Maximum Number is: "+result2);
     }
 }
